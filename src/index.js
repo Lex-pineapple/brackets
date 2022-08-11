dcaceaf988abcb1a6d3b120043e23cd7438207e5
Lex <pineapplepear55@gmail.com>
2022-08-11 10:19:53 +0300

    feat: add main functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brConf = bracketsConfig;
+  let brStr = str.split("");
+  console.log(brConf);
+  console.log(brStr);
+  let brCycler = () => {
+    for (let i = 0; i < brConf.length; i++) {
+      for (let k = 0; k < brStr.length; k++) {
+        while (brStr[k] == brConf[i][0] && brStr[k+1] == brConf[i][1]) {
+          brStr.splice(k, 2);
+          brCycler();
+        }
+      }
+    }
+  }
+  brCycler();
+  return brStr.length > 0 ? false : true;
 }
